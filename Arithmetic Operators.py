70ee6d323db0f2092fcea0001fd1460137ea7439
Mehul Solanki <er.mehulsolanki1887@gmail.com>
2024-04-19 16:44:00 +0530

    update file

--- Arithmetic Operators.py
+++ Arithmetic Operators.py
@@ -1,12 +1,10 @@
-# Logical Operators
-a = 50
-b = 30
+# Arithmatic Operators
+a = 5
+b = 2
 
-print(not False)
-print(not (a > b))
-
-val1 = False
-val2 = False
-
-print("AND operator:" , val1 and val2)
-print("OR operator:" , (a == b) or (a > b))
+print(10 + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a % b) 
+print(a ** b)
